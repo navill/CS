1211defde680c22493d0ebe0039a907d6b2ad168
taehwan <ythwork83@gmail.com>
2018-10-06 17:47:36 +0900

    polynomial

--- data_structure/linked_list/polynomial.py
+++ data_structure/linked_list/polynomial.py
@@ -1,4 +1,5 @@
 import math
+import copy
 
 class PolyNode:
     def __init__(self, coefficient, exponent):
@@ -90,13 +91,44 @@ class PolyList:
 
         return temp
 
+    def __sub__(self, other):
+        p1 = self
+        p2 = copy.deepcopy(other)
+        p2_it = p2.head.link
+        while p2_it:
+            p2_it.coef = -p2_it.coef
+            p2_it = p2_it.link
+        return p1 + p2
+
+    def __mul__(self, other):
+        p1 = self; p2 = other
+        poly_list = []
+        p1_it = p1.head.link
+
+        while p1_it:
+            p2_it = p2.head.link
+            temp_poly = PolyList()
+            while p2_it:
+                coef = p1_it.coef * p2_it.coef
+                exp = p1_it.exp + p2_it.exp
+                temp_poly.attach(coef, exp)
+                p2_it = p2_it.link
+            poly_list.append(temp_poly)
+            p1_it = p1_it.link
+        
+        res = PolyList()
+        for p in poly_list:
+            res += p
+
+        return res
+
 if __name__ == "__main__":
     p1 = PolyList()
     p2 = PolyList()
 
-    #p1 = 3x^3 + 1x^2      + 6
+    #p1 = 3x^3 + 1x^2      + 3
     p1.attach(3, 3)
-    p1.attach(6, 0)
+    p1.attach(3, 0)
     p1.attach(1, 2)
     PolyList.print_poly(p1)
 
@@ -106,6 +138,32 @@ if __name__ == "__main__":
     p2.attach(12, 0)
     PolyList.print_poly(p2)
 
-    #res = 5x^3 + x^2 + 7x + 18
+    #res = 5x^3 + x^2 + 7x + 15
+    print('polynomial addition')
     res = p1 + p2
+    PolyList.print_poly(res)
+    print()
+
+    #res = x^3 + x^2 + -7x^1 - 9
+    print('polynomial subtraction')
+    res = p1 - p2
+    PolyList.print_poly(res)
+    print()
+
+    print("polynomial multiplication")
+    #p3 = 3x - 2
+    p3 = PolyList()
+    p3.attach(3, 1)
+    p3.attach(-2, 0)
+    PolyList.print_poly(p3)
+
+    #p4 = 2x^2 -2x +3 
+    p4 = PolyList()
+    p4.attach(2, 2)
+    p4.attach(-2, 1)
+    p4.attach(3, 0)
+    PolyList.print_poly(p4)
+
+    #res = 6x^3 + -10x^2 + 13x^1 + -6
+    res = p3 * p4
     PolyList.print_poly(res)
