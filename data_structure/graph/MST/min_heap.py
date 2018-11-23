626efe6291d85345184b0df47a2e041f0c9150e1
taehwan <ythwork83@gmail.com>
2018-11-23 17:36:40 +0900

    minimum cost spanning tree modified

--- data_structure/graph/MST/min_heap.py
+++ data_structure/graph/MST/min_heap.py
@@ -9,6 +9,8 @@ class MinHeap:
     def __init__(self):
         self.arr=[None for i in range(self.MAX_ELEMENTS)]
         self.heapsize=0
+        #정점이 arr에 위치한 현재 인덱스
+        self.pos=[None for i in range(self.MAX_ELEMENTS)]
 
     def is_empty(self):
         if self.heapsize==0:
@@ -39,9 +41,16 @@ class MinHeap:
         #cur_idx가 루트가 아니고
         #item의 w가 cur_idx 부모의 키보다 작으면
         while cur_idx!=1 and item.w < self.arr[self.__get_parent_idx(cur_idx)].w:
+            #리프 노드로 추가된 새로운 원소의 weight가 부모의 원소의 weight보다 
+            #더 작으면 부모 원소를 한 칸 내린다 
             self.arr[cur_idx]=self.arr[self.__get_parent_idx(cur_idx)]
+            #아래로 내려오는 부모 원소의 위치 인덱스 업데이트
+            self.pos[self.arr[cur_idx].v]=cur_idx
+
             cur_idx=self.__get_parent_idx(cur_idx)
+
         self.arr[cur_idx]=item
+        self.pos[item.v]=cur_idx
 
     def __get_smaller_child_idx(self, idx):
         if self.heapsize < self.__get_left_child_idx(idx):
@@ -71,11 +80,18 @@ class MinHeap:
         smaller_child_idx=self.__get_smaller_child_idx(cur_idx)
 
         while smaller_child_idx and temp.w > self.arr[smaller_child_idx].w:
+            #마지막 원소보다 weight가 큰 정점은 루트쪽으로 한칸 올라간다
             self.arr[cur_idx]=self.arr[smaller_child_idx]
+            
+            #이와 함께 루트쪽으로 올라간 정점의 현재 인덱스도 업데이트한다
+            self.pos[self.arr[cur_idx].v]=cur_idx
+
             cur_idx=smaller_child_idx
             smaller_child_idx=self.__get_smaller_child_idx(cur_idx)
         
         self.arr[cur_idx]=temp
+        self.pos[temp.v]=cur_idx
+
         self.heapsize-=1
 
         return rem_elem
@@ -88,15 +104,24 @@ class MinHeap:
 
     #프림 알고리즘을 위해 추가된 함수
     def decrease_weight(self, new_elem):
-        for i in range(1, self.heapsize+1):
-            if self.arr[i].v==new_elem.v:
-                cur=i
-                break
+        #업데이트될 정점의 현재 인덱스
+        cur=self.pos[new_elem.v]
 
+        #cur가 루트가 아니고 업데이트 될 원소의 weight가
+        #부모 원소의 weight보다 작다면 부모 원소를 아래로 내리고
+        #cur가 루트 쪽으로 올라간다
         while cur!= 1 and new_elem.w < self.arr[self.__get_parent_idx(cur)].w:
+            #업데이트 될 원소의 weight가 부모 원소의 weight보다 작다면
+            #부모 원소를 한 칸 아래로 내린다 
             self.arr[cur]=self.arr[self.__get_parent_idx(cur)]
+
+            #내려온 원소의 위치 인덱스 업데이트
+            self.pos[self.arr[cur].v]=cur    
+
             cur=self.__get_parent_idx(cur)
+
         self.arr[cur]=new_elem
+        self.pos[new_elem.v]=cur
 
 def print_heap(h):
     for i in range(1, h.heapsize+1):
