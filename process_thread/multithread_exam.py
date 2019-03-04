085c4ed9dff8fa2d49cd95d0c05ecfc53ced5996
ythwork <ythwork83@gmail.com>
2019-03-04 13:43:01 +0900

    multithreading

--- process_thread/multithread_exam.py
+++ process_thread/multithread_exam.py
@@ -1,19 +1,16 @@
 import threading
-import time
 
 def thread_main(li, n):
     for i in range(offset * n + 1, offset *(n + 1) + 1):
         li[i] *= 2
 
 n = 1000 
-#n = 60000000
 
 offset = n // 4
 
 li = [i for i in range(n+1)]
 threads = []
 
-# start=time.time()
 for i in range(4):
     th = threading.Thread(target = thread_main,
                           args = (li, i))
@@ -25,7 +22,5 @@ for th in threads:
 for th in threads:
     th.join()
 
-# elapsed=time.time()-start
-# print(f'elapsed time : {elapsed:0.2f})
 print(li)
 
