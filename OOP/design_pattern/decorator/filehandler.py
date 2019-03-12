037fdac390c9851ee924c41c494fa96ddb892eab
greg <ythwork83@gmail.com>
2019-03-12 23:09:17 +0900

    decorator

--- OOP/design_pattern/decorator/filehandler.py
+++ OOP/design_pattern/decorator/filehandler.py
@@ -4,7 +4,7 @@ from shutil import copyfile
 import os
 import zipfile
 import sys
-
+from Crypto.Cipher import AES
 
 # add functionality to the method that I make.
 
@@ -59,12 +59,37 @@ class ZipFileCopier(FileDecorator):
 		self.real_copy(new_src, dst)
 		os.remove(new_src)		
 
-class EncryptedCopier(FileDecorator):
+class EncryptCopier(FileDecorator):
 	def __init__(slf, delegate):
 		super().__init__(delegate)
 
-	def encrypt_file(src, dst):
-		pass
+	def encrypt(self, content):
+		obj=AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+		len_of_content=len(content)
+		lack_bytes=16-(len_of_content%16)
+		new_content=content+'*'*lack_bytes
+		ciphertext=obj.encrypt(new_content)
+
+		return ciphertext, lack_bytes
+	
+	def encrypt_file(self, src, dst):
+		# read from the src file	
+		org_f=open(src, 'r')
+		content=org_f.read()
+		org_f.close()
+
+		# encrypt
+		ciphertext, lack_bytes=self.encrypt(content)
+
+		# write to the new_src file	
+		new_src='new_'+os.path.split(src)[1]	
+		new_file=open(new_src, 'wb')
+		new_file.write(lack_bytes.to_bytes(1, 'little', signed=False))
+		new_file.write('\n'.encode())
+		new_file.write(ciphertext)
+		new_file.close()
+
+		return new_src
 
 	def copy(self, src, dst):
 		# added functionality
@@ -75,8 +100,11 @@ class EncryptedCopier(FileDecorator):
 
 if __name__=="__main__":
 	rfc=RealFileCopier()
-	zfc=ZipFileCopier(rfc)
-	zfc.copy(sys.argv[1], sys.argv[2])
+#	zfc=ZipFileCopier(rfc)
+#	zfc.copy(sys.argv[1], sys.argv[2])
+	
+	efc=EncryptCopier(rfc)
+	efc.copy(sys.argv[1], sys.argv[2])
 
 	
 
