1b5ca752173aef7417730c7bbebb81a00aef91d7
taehwan <ythwork83@gmail.com>
2019-03-26 22:40:09 +0900

    insertion_sort

--- data_structure/sort/bubble_sort/bubble_sort.py
+++ data_structure/sort/bubble_sort/bubble_sort.py
@@ -1,5 +1,4 @@
 import random
-import time
 
 def bubble_sort(li):
     n=len(li)
@@ -13,9 +12,7 @@ if __name__=="__main__":
         num_data=int(input('데이터 개수(종료:0):'))
         if not num_data:
             break
-        data=[random.randint(1, 1000) for _ in range(num_data)]
-        start=time.time()
+        data=[random.randint(1, 100) for _ in range(num_data)]
+        print(data)
         bubble_sort(data)
-        elapsed=time.time()-start
-        print('elapsed time : {} sec.'.format(elapsed))
-        #print(data)
+        print(data)
---
+++ data_structure/sort/insertion_sort/insertion_sort.py
@@ -0,0 +1,23 @@
+import random
+
+def insertion_sort(li):
+    n=len(li)
+    temp=None
+    for i in range(1, n):
+        temp=li[i]
+        for j in range(i-1, -1, -1):
+            if li[j] > temp:
+                li[j+1]=li[j]
+            else:
+                break
+        li[0 if j==0 else j+1]=temp
+
+if __name__=="__main__":
+    while True:
+        num_data=int(input('데이터 개수(종료:0):'))
+        if not num_data:
+            break
+        data=[random.randint(1, 100) for _ in range(num_data)]
+        print(data)
+        insertion_sort(data)
+        print(data)
