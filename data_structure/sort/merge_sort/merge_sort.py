47ee8b05e45dbdeaca8aec944a2361be42c3385b
taehwan <ythwork83@gmail.com>
2019-03-26 23:54:17 +0900

    merge_sort

--- data_structure/sort/merge_sort/merge_sort.py
+++ data_structure/sort/merge_sort/merge_sort.py
@@ -1,8 +1,41 @@
 import random
 
+def merge_sort_recursion(li, merged, start, end):
+    if start >= end:
+        return
+
+    mid=(start+end)//2
+    merge_sort_recursion(li, merged, start, mid)
+    merge_sort_recursion(li, merged, mid+1, end)
+
+    left_idx=start; right_idx=mid+1; temp_idx=start
+    while left_idx<=mid and right_idx<=end:
+        if li[left_idx]<li[right_idx]:
+            merged[temp_idx]=li[left_idx]
+            left_idx+=1
+            temp_idx+=1
+        else:
+            merged[temp_idx]=li[right_idx]
+            right_idx+=1
+            temp_idx+=1
+    while left_idx<=mid:
+        merged[temp_idx]=li[left_idx]
+        left_idx+=1
+        temp_idx+=1
+    while right_idx<=end:
+        merged[temp_idx]=li[right_idx]
+        right_idx+=1
+        temp_idx+=1
+    for i in range(start, end+1):
+        li[i]=merged[i]
+
 
 def merge_sort(li):
-    pass
+    n=len(li)
+    merged=[None for _ in range(n)]
+    start=0
+    end=n-1
+    merge_sort_recursion(li, merged, start, end)
 
 if __name__=="__main__":
     while True:
