0ef040cfab49c269b56fe318563fa46499261cbb
ythwork <ythwork83@gmail.com>
2019-04-03 23:09:05 +0900

    echo

--- network/socket_programming/echo/echo_client.py
+++ network/socket_programming/echo/echo_client.py
@@ -1,11 +1,20 @@
+import sys
 import socket
 
-serv_ip, serv_port='127.0.0.1', 3030
+buf=None
+serv_ip, serv_port=sys.argv[1], int(sys.argv[2])
 
 clnt_sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 clnt_sock.connect((serv_ip, serv_port))
 
-clnt_sock.send(b'hello, you should learn computer science')
-received=clnt_sock.recv(1024)
-print(received)
+while True:
+	msg=input("# ")
+	if msg=='0':
+		break
+	buf=msg.encode()
+	clnt_sock.send(buf)
+	b_received=clnt_sock.recv(1024)
+	received=b_received.decode()
+	print(f'received : {received}')	
+	
--- network/socket_programming/echo/echo_server.py
+++ network/socket_programming/echo/echo_server.py
@@ -1,19 +1,21 @@
 import socket
+import sys
 
 serv_sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv_sock.bind(('127.0.0.1', 3030))
+
+serv_sock.bind((sys.argv[1], int(sys.argv[2])))
 
 serv_sock.listen()
 
 data_sock, clnt_addr=serv_sock.accept()
 
 if data_sock:
-    while True:
-        received=data_sock.recv(1024)
-        if not received:
-            break
-        print('received from client : {}'.format(received))
-        data_sock.sendall(received)
+	while True:
+		received=data_sock.recv(1024)
+		if not received:
+			break
+		print(f'received from client :{received}')
+		data_sock.sendall(received)
 
 data_sock.close()
 serv_sock.close()
