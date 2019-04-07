9d02a6ab431a1d50fefe1a557e7ae0b892de0c02
ythwork <ythwork83@gmail.com>
2019-04-08 01:06:00 +0900

    framework

---
+++ database/python_mysql/fw_db.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.17  Distrib 10.3.13-MariaDB, for osx10.14 (x86_64)
+--
+-- Host: localhost    Database: fw_db
+-- ------------------------------------------------------
+-- Server version	10.3.13-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `fw_db`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `fw_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `fw_db`;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (1,'극한직업','매우 유쾌한 영화. 아무 생각 없이 웃고 싶다면 추천!'),(2,'어벤저스4','다음 편이 정말 기대되는 영화!!!!'),(4,'스파이더맨 뉴 유니버스','놀랍도록 잘 만든 영화! 박수 갈채 필수! 5점이 아깝다!!!'),(6,'완벽한 타인','올해 가장 재밌게 본 영화 중 하나! 꽤 인상적이었음');
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-08  0:53:38
--- framework/db.py
+++ framework/db.py
@@ -1,70 +1,99 @@
-import psycopg2 as pg2
+from mysql.connector import MySQLConnection
+from configparser import ConfigParser
 
-class DbManager:
-    conn = None
-    cursor = None
+class SingletonError(Exception):
+    pass
+
+class DBManager:
+    dbmanager=None
+
+    @classmethod
+    def get_instance(cls):
+        return cls.dbmanager
+    
+    @staticmethod
+    def get_conf(filename='conf.ini'):
+        parser=ConfigParser()
+        parser.read(filename)
+        conf={}
+        if parser.has_section('mysql'):
+            items=parser.items('mysql')
+            for key, value in items:
+                conf[key]=value
+            return conf
+        return None
+
     def __init__(self):
-        DbManager.conn = pg2.connect(database='wsgi_db', user='django',
-                            password='PythonMaster')
-        DbManager.cursor = DbManager.conn.cursor()
+        if DBManager.dbmanager:
+            raise SingletonError("There is already another DBManager instance")
 
+        conf=DBManager.get_conf()
+        self.conn=MySQLConnection(**conf)
+
+    def init(self):
+        DBManager.dbmanager=self
+        
     def __del__(self):
-        DbManager.cursor.close()
-        DbManager.conn.close()
+        self.conn.close()
 
     def get_queryset(self):
-        DbManager.cursor.execute(
+        cursor=self.conn.cursor()
+        cursor.execute(
         """
         SELECT *
         FROM post;
         """
         )
-        query_set = DbManager.cursor.fetchall()
+        query_set = cursor.fetchall()
+        cursor.close()
+
         return query_set
 
     def retrieve_post(self, id):
-        DbManager.cursor.execute(
+        cursor=self.conn.cursor()
+        cursor.execute(
         """
         SELECT *
         FROM post
         WHERE id=%s
         """, (id,))
-        return DbManager.cursor.fetchone()
+        record=cursor.fetchone()
+        cursor.close()
+        
+        return record
 
     def create_post(self, title, content):
-        DbManager.cursor.execute(
+        cursor=self.conn.cursor()
+        cursor.execute(
         """
-        SELECT *
-        FROM post
-        ORDER BY id DESC
-        LIMIT 1
-        """
-        )
-        id = DbManager.cursor.fetchone()[0]
-        id += 1
-
-        DbManager.cursor.execute(
-        """
-        INSERT INTO post
-        VALUES(%s, %s, %s);
-        """, (id, title, content))
-        DbManager.conn.commit()
+        INSERT INTO post 
+        (title, content)
+        VALUES(%s, %s);
+        """, (title, content))
+        self.conn.commit()
+        cursor.close()
 
     def update_post(self, id, title, content):
-        DbManager.cursor.execute(
+        cursor=self.conn.cursor()
+        cursor.execute(
         """
         UPDATE post
         SET title= %s, content=%s
         WHERE id=%s;
         """
-        , (title, content, id))
+        , (title, content, str(id)))
+        self.conn.commit()
+        cursor.close()
 
     def delete_post(self, id):
-        DbManager.cursor.execute(
+        cursor=self.conn.cursor()
+        cursor.execute(
         """
         DELETE FROM post
         WHERE id=%s;
-        """, (id, ))
-        DbManager.conn.commit()
+        """, (str(id), ))
+        self.conn.commit()
+        cursor.close()
 
-db_manager = DbManager()
+db_manager = DBManager()
+db_manager.init()
